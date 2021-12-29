46f7ed735a35c4fb7d8d9aa02ae15c5723006790
Artur Sadrtdinov <79194435+asadrtdinov@users.noreply.github.com>
2021-12-29 16:13:20 +0500

    fix: cors and logger

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,5 +1,11 @@
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-const ALLOWED_CORS = ['http://localhost:3000'];
+const ALLOWED_CORS = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+  'http://asadrtdinov.nomoredomains.rocks/',
+  'https://asadrtdinov.nomoredomains.rocks/',
+];
 
 module.exports = (req, res, next) => {
   const { method } = req;
