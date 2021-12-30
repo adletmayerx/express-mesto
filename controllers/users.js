9aa2bb5c293487580e9d90f3d513c80d58d812bb
Artur Sadrtdinov <79194435+asadrtdinov@users.noreply.github.com>
2021-12-30 18:30:21 +0500

    fix: cors

--- controllers/users.js
+++ controllers/users.js
@@ -129,7 +129,8 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: true,
+          sameSite: false,
+          secure: false,
         })
         .send({ message: 'Логин успешный' });
     })
