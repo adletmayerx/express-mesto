810d10a9d9293a0a408dd2e1f575a100a6158d7c
Artur Sadrtdinov <79194435+asadrtdinov@users.noreply.github.com>
2021-12-31 09:42:53 +0500

    fix: cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const cors = require('cors');
+// const cors = require('cors');
 const { createUser, login, signOut } = require('./controllers/users');
 const { userValidation, loginValidation } = require('./middlewares/validation');
 const auth = require('./middlewares/auth');
@@ -16,28 +16,32 @@ require('dotenv').config();
 const { PORT = 3000, BASE_PATH } = process.env;
 const app = express();
 
-const corsAllowed = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'localhost:3000',
-  'http://asadrtdinov.nomoredomains.rocks',
-  'https://asadrtdinov.nomoredomains.rocks',
-];
+// const corsAllowed = [
+//   'https://praktikum.tk',
+//   'http://praktikum.tk',
+//   'localhost:3000',
+//   'http://asadrtdinov.nomoredomains.rocks',
+//   'https://asadrtdinov.nomoredomains.rocks',
+// ];
 
-app.use(
-  cors({
-    credentials: true,
-    origin(origin, callback) {
-      if (corsAllowed.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  }),
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin(origin, callback) {
+//       if (corsAllowed.includes(origin) || !origin) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//   }),
+// );
 
-app.options('*', cors());
+// app.options('*', cors());
+
+app.options((req, res) => {
+  res.status(200).header('Access-Control-Allow-Headers', '*').header('Access-Control-Allow-Origin', '*').end();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
